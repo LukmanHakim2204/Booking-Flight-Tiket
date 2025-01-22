1101f1e9e3ecff1fe85434bbdf5d3959557d0318
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-22 23:18:42 +0700

    make migration booking

---
+++ database/migrations/2025_01_22_034427_create_flight_segments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flight_segments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sequence');
+            $table->foreignId('flight_id')->references('id')->on('flights');
+            $table->foreignId('airport_id')->references('id')->on('airports');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flight_segments');
+    }
+};
