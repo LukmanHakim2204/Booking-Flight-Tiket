2197b7849e762a33f7283579de98feb7db6be083
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-22 23:25:18 +0700

    create model airport

--- database/migrations/2025_01_22_033538_create_airports_table.php
+++ database/migrations/2025_01_22_033538_create_airports_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image');
             $table->string('city');
             $table->string('country');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
